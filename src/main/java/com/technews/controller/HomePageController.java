1ace9226c4bb59dc196185df092dd86a3e6e3e24
Michelle Smith <smith.michelle.a@gmail.com>
2021-05-17 15:32:44 -0400

    added HomePage Controller

---
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -0,0 +1,35 @@
+package com.technews.controller;
+
+@Controller
+public class HomePageController {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @GetMapping("/login")
+    public String login(Model model, HttpServletRequest request) {
+
+        if (request.getSession(false) != null) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @GetMapping("/users/logout")
+    public String logout(HttpServletRequest request) {
+        if (request.getSession(false) != null) {
+            request.getSession().invalidate();
+        }
+        return "redirect:/login";
+    }
+}
